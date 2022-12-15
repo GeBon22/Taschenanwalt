a560d709d3f93a1e34e2a8139b193322ea5a3f4a
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-12-15 12:59:58 +0100

    added lawyer display function

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, useLoadScript } from '@react-google-maps/api';
 import { useEffect, useState, React } from "react";
 
 const containerStyle = {
@@ -10,7 +10,11 @@ const containerStyle = {
 export default function Map() {
   const [status, setStatus] = useState(null);
   const [center, setCenter] = useState(null);
+  const [lawyers, setLawyers] = useState([]);
   const ApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: ApiKey,
+  });
 
   useEffect(() => {
     if (!navigator.geolocation) {
@@ -24,6 +28,7 @@ export default function Map() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           });
+          loadLawyers(position.coords.latitude, position.coords.longitude);
         },
         () => {
           setStatus("Locating failed");
@@ -31,6 +36,27 @@ export default function Map() {
       );
     }
   }, []);
+
+  function loadLawyers(lat, lng) {
+    // Load the lawyers using the Google Maps Places function
+    if (isLoaded && !loadError) {
+      const service = new window.google.maps.places.PlacesService(document.createElement('div'));
+      const request = {
+        location: new window.google.maps.LatLng(lat, lng),
+        radius: 50000, // 50 km
+        type: ['lawyer'],
+      };
+      service.nearbySearch(request, (results, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          setLawyers(results.map(result => ({
+            name: result.name,
+            lat: result.geometry.location.lat(),
+            lng: result.geometry.location.lng(),
+          })));
+        }
+      });
+    }
+  }
 
   if (status === "Locating failed") {
     return <h3>{status}</h3>;
@@ -47,6 +73,13 @@ export default function Map() {
             zoom={12}
           >
             <Marker position={center} />
+
+            {lawyers.map(lawyer => (
+            <Marker key={lawyer.name} position={{ lat: lawyer.lat, lng: lawyer.lng }} />
+            ))}
+
+
+
             { /* Child components, such as markers, info windows, etc. */ }
             <></>
           </GoogleMap>
