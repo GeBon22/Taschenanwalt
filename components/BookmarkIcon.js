76f90a8f41167ea1455b4c4b4d3d6e90ee2919d6
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-12-28 15:55:21 +0100

    added bookmark sucess notification

--- components/BookmarkIcon.js
+++ components/BookmarkIcon.js
@@ -1,9 +1,22 @@
 import {Icon} from "@iconify/react";
 import styled from "styled-components";
 import {useRouter} from "next/router";
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function useSavePage() {
   const {pathname} = useRouter();
+  const notify = () =>
+    toast.success("gespeichert!", {
+      position: "top-right",
+      autoClose: 2500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
   function savePage() {
     let bookmarks = JSON.parse(localStorage.getItem("bookmarks") || "[]");
@@ -17,6 +30,7 @@ function useSavePage() {
     }
 
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    notify();
   }
 
   return savePage;
@@ -26,11 +40,14 @@ function BookmarkIcon() {
   const savePage = useSavePage();
 
   return (
-    <StyledIcon
-      icon="mingcute:bookmark-fill"
-      color="#572887"
-      onClick={savePage}
-    />
+    <>
+      <StyledIcon
+        icon="mingcute:bookmark-fill"
+        color="#572887"
+        onClick={savePage}
+      />
+      <ToastContainer />
+    </>
   );
 }
 
