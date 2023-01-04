968fb7f7b49f59c60404372a6a5739cfb9396871
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2023-01-04 10:45:46 +0100

    added and styled new header

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,29 @@
 import styled from "styled-components";
-import lottie from "lottie-web";
-import {useEffect, useRef} from "react";
+import Logo from "../public/assets/Logo.png";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
-  const container = useRef(null);
 
-  useEffect(() => {
-    lottie.loadAnimation({
-      container: container.current,
-      render: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: require("/public/HeaderAnimation.json"),
-    });
-    return () => lottie.destroy();
-  });
 
   return (
     <StyledLink href="/">
       <StyledHeader>
-        <Styledh3>Taschen</Styledh3>
-        <AnimationContainer ref={container}></AnimationContainer>
-        <Styledh3>Anwalt</Styledh3>
+      <Image src={Logo} alt="Taschenanwalt" width={270} height={90} />
       </StyledHeader>
     </StyledLink>
   );
 }
 
-const AnimationContainer = styled.div`
-  width: 70px;
-  height: 90px;
-`;
-
 const StyledHeader = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 5px;
+  padding: 1rem;
   box-shadow: var(--box-shadow);
   background-color: var(--primary-background-color);
   color: var(--primary-color);
   text-decoration: none;
-  border-bottom: 1px solid var(--primary-color);
-`;
-
-const Styledh3 = styled.h3`
-  text-decoration: none;
-  font-size: 1.5rem;
 `;
 
 const StyledLink = styled(Link)`
--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -20,7 +20,6 @@ const GlobalStyles = createGlobalStyle`
     --primary-background-color: #303649;
     --default-border-radius: 10px;
     --button-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    /* --button-background: #e6ceee; */
     --box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
