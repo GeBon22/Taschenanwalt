5b50399f7fcbb03bdcaf4a6c39604cf5c21fbe28
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2023-01-04 16:32:07 +0100

    changed nav padding

--- components/Navigation.js
+++ components/Navigation.js
@@ -33,7 +33,7 @@ const StyledNav = styled.nav`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  padding: 20px;
+  padding: 0.8rem;
   box-shadow: var(--box-shadow);
   background-color: var(--primary-color);
   position: sticky;
